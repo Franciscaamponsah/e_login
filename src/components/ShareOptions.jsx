d168ed673abfa25d7a98bbb6921ea2ac1b759ef1
Franciscaamponsah <franciscaamponsah24@gmail.com>
2024-07-17 17:38:48 +0000

    Added a hover to the Share Options

--- src/components/ShareOptions.jsx
+++ src/components/ShareOptions.jsx
@@ -2,22 +2,22 @@ import React from 'react'
 
 const ShareOptions = () => {
   return (
-    <ul className='pl-10'>
-      <li className="mb-2">Shareholders </li>
-      <li className="mb-2">Approved Pending Edits</li>
-      <li className="mb-2">Print ID</li>
-      <li className="mb-2">Staff Share Holders </li>
-      <li className="mb-2">Member Share Holders </li>
-      <li className="mb-2">Director Share Holders </li>
-      <li className="mb-2">Family Share Holders </li>
-      <li className="mb-2">Cooperate Share Holders </li>
-      <li className="mb-2">Management Share Holders </li>
-      <li className="mb-2">Ex-Staff Share Holders </li>
-      <li className="mb-2">Ex-Director Share Holders </li>
-      <li className="mb-2">New Share Holders</li>
-      <li className="mb-2">Import Share Holders</li>
-      <li className="mb-2">Combine Accounts</li>
-      <li className="mb-2">Accounts Combine</li>
+    <ul className="pl-10">
+      <li className="mb-2  hover:bg-[#EAECF0]">Shareholders </li>
+      <li className="mb-2  hover:bg-[#EAECF0]">Approved Pending Edits</li>
+      <li className="mb-2  hover:bg-[#EAECF0]">Print ID</li>
+      <li className="mb-2  hover:bg-[#EAECF0]">Staff Share Holders </li>
+      <li className="mb-2  hover:bg-[#EAECF0]">Member Share Holders </li>
+      <li className="mb-2  hover:bg-[#EAECF0]">Director Share Holders </li>
+      <li className="mb-2  hover:bg-[#EAECF0]">Family Share Holders </li>
+      <li className="mb-2  hover:bg-[#EAECF0]">Cooperate Share Holders </li>
+      <li className="mb-2  hover:bg-[#EAECF0]">Management Share Holders </li>
+      <li className="mb-2  hover:bg-[#EAECF0]">Ex-Staff Share Holders </li>
+      <li className="mb-2  hover:bg-[#EAECF0]">Ex-Director Share Holders </li>
+      <li className="mb-2  hover:bg-[#EAECF0]">New Share Holders</li>
+      <li className="mb-2  hover:bg-[#EAECF0]">Import Share Holders</li>
+      <li className="mb-2  hover:bg-[#EAECF0]">Combine Accounts</li>
+      <li className="mb-2  hover:bg-[#EAECF0]">Accounts Combine</li>
     </ul>
   );
 }
